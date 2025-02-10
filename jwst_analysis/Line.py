2304650fbff3702b7ece4c48eb8151e967033475
Blake Drechsler <wbd814@gmail.com>
2025-02-10 16:37:49 -0500

    Added attributes to line object

--- jwst_analysis/Line.py
+++ jwst_analysis/Line.py
@@ -1,3 +1,6 @@
 class Line:
-    def __init__(self, rest_wvl):
+    def __init__(self, rest_wvl, species, transition, line_width=0.013):
         self.rest_wvl = rest_wvl
+        self.sepcies = species
+        self.transition = transition
+        self.line_width = line_width
