cccb9128fded1355fbcc24027ed78eee42c54423
Blake Drechsler <wbd814@gmail.com>
2025-02-11 10:53:24 -0500

    fixed typo

--- jwst_analysis/Line.py
+++ jwst_analysis/Line.py
@@ -1,7 +1,7 @@
 class Line:
     def __init__(self, rest_wvl, species, transition, line_width=0.013):
         self.rest_wvl = rest_wvl
-        self.sepcies = species
+        self.species = species
         self.transition = transition
         self.line_width = line_width
 
